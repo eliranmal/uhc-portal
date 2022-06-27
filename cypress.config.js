3a2dd0a63689940b52d9059da50e2bec5feb12e3
David Taylor <user@example.com>
2022-06-27 11:40:38 -0400

    code coverage

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
     },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
